8859fa4cd6c3778ad5fdf64d474d43d2a13cb403
CatalinaBoacas <boacas.catalina@yahoo.com>
2016-11-20 18:26:19 +0200

    Laborator 6- Exercitiul 1 si 2

---
+++ UbbCourse6/ProjectManagement/ProjectManagement/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using ProjectManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+           // ViewBag.Message = "Your application description page.";
+            //ViewBag.Author = "Catalina Boacas";
+            var model = new AboutModel();
+            model.Message = "Your application description page. Test";
+            model.Author = "Catalina Boacas";
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
