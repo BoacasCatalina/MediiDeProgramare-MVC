7f8c6b2c25518d31606ab870e6b29473b1e9f39b
CatalinaBoacas <boacas.catalina@yahoo.com>
2017-01-22 21:43:04 +0200

    Laborator 9 - exercitiul 3

--- UbbCourse9/RazorProject/RazorProject/Models/ProjectEvaluation.cs
+++ UbbCourse9/RazorProject/RazorProject/Models/ProjectEvaluation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,9 +10,13 @@ namespace RazorProject.Models
     [Bind(Exclude = "country")]
     public class ProjectEvaluation
     {
+        [Required]
+        [StringLength(1000)]
         public string name { get; set; }
         public string city { get; set; }
         public string country { get; set; }
+        [Range(1, 10)]
+        [Required]
         public double rating { get; set; }
 
         public int id { get; set; }
