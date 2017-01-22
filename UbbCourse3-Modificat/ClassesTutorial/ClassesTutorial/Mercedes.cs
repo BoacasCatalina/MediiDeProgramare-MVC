b5a26a1118581626839991f0baa090dba4f982f6
CatalinaBoacas <boacas.catalina@yahoo.com>
2017-01-22 18:50:47 +0200

    Laborator 3 - Exercitiile 1-4

---
+++ UbbCourse3-Modificat/ClassesTutorial/ClassesTutorial/Mercedes.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesTutorial
+{
+    internal class Mercedes:AbstractCar
+    {
+        public string Description;
+        public int Price;
+        private string _description;
+        private int _price;
+        protected string colour;
+        protected int year;
+        internal string mark;
+        internal int age;
+        public Mercedes()
+        {
+            Description = "E Class";
+            Price = 67000;
+        }
+
+        public string _Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                _description = "My first car";
+            }
+        }
+
+        public int _Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                _price = 50;
+            }
+        }
+
+        public void StartEngine()
+        {
+            Console.WriteLine("Engine started");
+            //return "Engine started";
+        }
+
+        public void ShowRemainingNoOfKM(int noOfFuelLiters, int Consumption)
+        {
+            Console.WriteLine("Remaining km: " + noOfFuelLiters / Consumption * 100);
+            //return noOfFuelLiters / Consumption * 100;
+        }
+
+        public string StartEngine(int noOfMiliseconds)
+        {
+                return "The car is starting in {" + noOfMiliseconds + "}";
+            
+        }
+
+        public double CalculateConsumption(params int[] kms)
+        {
+            double sum = 0;
+            foreach (int km in kms)
+                sum += km;
+            return sum / 100 * 7;
+        }
+    }
+}
---
+++ UbbCourse3-Modificat/ClassesTutorial/ClassesTutorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesTutorial
+{
+    struct EngineStruct { };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // AbstractCar aCar = new AbstractCar(); Nu se poate instantia
+            Mercedes mercedes = new Mercedes();
+            EngineStruct engine = new EngineStruct();
+            mercedes.StartEngine();
+            mercedes.ShowRemainingNoOfKM(100, 50);
+            ICar car = new Mercedes();
+            AbstractCar car2 = new Mercedes();
+            Console.WriteLine("This is my first application");
+            Console.WriteLine("Mercedes is " + mercedes.Description);
+            Console.WriteLine("Mercedes costs " + mercedes.Price);
+            Console.WriteLine(mercedes.StartEngine(1000));
+            Console.WriteLine("The consumption is " + mercedes.CalculateConsumption(12, 25, 35));
+            Console.ReadKey();
+            
+        }
+    }
+}
